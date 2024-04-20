14e0da6eddcbb90f70fcba8269d4f68a580520f1
profeluli <115101305+profeluli@users.noreply.github.com>
2024-04-20 09:08:46 -0300

    dsadasdasdas

--- main.js
+++ main.js
@@ -5,4 +5,4 @@ console.log(nomeUsuario);
 console.log('Bom dia, "professores!"');
 console.log(`Seja bem vindo(a), ${nomeUsuario}!`);
 nomeUsuario = prompt("Digite o seu nome: ");
-console.log(`Seu nome é $(nomeUsuario)`);
+console.log(`Seu nome é ${(nomeUsuario)}`);
