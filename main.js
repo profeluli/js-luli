a8db1235b0b9901fe1ee31abe59c029b82f24b0d
profeluli <115101305+profeluli@users.noreply.github.com>
2024-04-20 10:07:56 -0300

    23662626

--- main.js
+++ main.js
@@ -48,7 +48,8 @@ console.log(`O resultado do resto é ${resto}`);
 
 //Estruturas condicionais
 
-let numero = 13;
+let numero;
+numero = prompt("Digite um número: ")
 if ((numero % 2) == 0){
     console.log("O número é par!")
 }else{
