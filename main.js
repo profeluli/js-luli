2d5a210c09bcd22d11f6557d58c8be51cf5d3615
profeluli <115101305+profeluli@users.noreply.github.com>
2024-04-20 09:18:06 -0300

    gyuguyguyguy

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 alert(1);
 //comentários são //
+
+//string
 let nomeUsuario = "Luli"
 console.log(nomeUsuario);
 console.log('Bom dia, "professores!"');
 console.log(`Seja bem vindo(a), ${nomeUsuario}!`);
 nomeUsuario = prompt("Digite o seu nome: ");
 console.log(`Seu nome é ${(nomeUsuario)}`);
+
+//números
+let idade = 30;
+console.log = (`Seu nome é ${nomeUsuario} e sua idade é ${idade}`);
