6068e8e99ad7f3421522e6a3eb83914826595ce2
profeluli <115101305+profeluli@users.noreply.github.com>
2024-04-20 09:28:48 -0300

    m,m,,m,mnjkk

--- main.js
+++ main.js
@@ -17,4 +17,4 @@ console.log(`Seu nome é ${nomeUsuario} e sua idade é ${idade}`);
 //número float
 let peso = 58.5;
 peso = prompt("Digite o seu peso: ");
-console.log(`Seu nome é ${nomeUsuario}, sua idade é ${idade} e seu peso é ${peso}`);
+console.log(`Seu nome é ${nomeUsuario}, sua idade é ${idade} e seu peso é ${peso} kg`);
