66cce461bc313ca9405337f5db3eea3e054caac7
profeluli <115101305+profeluli@users.noreply.github.com>
2024-04-20 10:54:41 -0300

    dsadasdsadsad

--- main.js
+++ main.js
@@ -64,3 +64,13 @@ console.log("O usuário é maior de idade")
 }else{
 console.log("Você não pode acessar esse site!")
 }
+
+//Operadores lógicos
+if (idade < 12){
+    console.log("criança")
+
+}else if(idade >= 12 && idade <= 40){
+console.log("Você é jovem!")
+}else (idade > 40){
+console.log("Adulto")
+}
